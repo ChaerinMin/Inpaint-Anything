070335025d321ccfab52c614f9b1845166643f42
Chaerin <c282828177@gmail.com>
2024-01-10 09:20:15 -0500

    initial

---
+++ script/remove_anything.sh
@@ -0,0 +1,14 @@
+python remove_anything.py \
+    --input_img ./example/remove-anything/00000030.jpg \
+    --coords_type key_in \
+    --point_coords 85 125 \
+    --point_labels 1 \
+    --dilate_kernel_size 15 \
+    --output_dir ./results \
+    --sam_model_type "vit_h" \
+    --sam_ckpt ./pretrained_models/sam_vit_h_4b8939.pth \
+    --lama_config ./lama/configs/prediction/default.yaml \
+    --lama_ckpt ./pretrained_models/big-lama
+
+
+    
